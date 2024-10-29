ea50f38e24ae382eeeb11d470b89272f9c90f6e0
zi11ion <118854607+zi11ion@users.noreply.github.com>
2024-10-29 15:39:15 -0400

    pathname change

---
+++ react-exercises/react-infosite/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
